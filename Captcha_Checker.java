226942edad60e3df44d757037fb2c7838abf22bb
aayushkumar20 <aayushkumarjsr16@protonmail.com>
2023-08-10 14:23:49 +0530

    some changes but still not working

--- Captcha_Checker.java
+++ Captcha_Checker.java
@@ -23,32 +23,55 @@
 // n = is the number of terms for checking the captcha
 // s = is the string for checking the captcha (it'll ask for n times)
 
+
 import java.util.*;
 
-class Captcha_Checker {
+class Captcha_Checker{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String[] s = new String[n];
-        for (int i = 0; i < n; i++) {
+        // Taking input for n times
+        for(int i=0; i<n; i++){
             s[i] = sc.next();
         }
-        for (int i = 0; i < n; i++) {
-            System.out.println(checkCaptcha(s[i]));
-        }
+        // Calling the function
+        boolean ans = captchaChecker(s, n);
+        System.out.println(ans);
     }
-
-    static boolean checkCaptcha(String s) {
-        int[] freq = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a']++;
-        }
-        Arrays.sort(freq);
-        for (int i = 0; i < 25; i++) {  // Corrected loop condition
-            if (freq[i] != freq[i + 1]) {
-                return false;
+    public static boolean captchaChecker(String[] s, int n){
+        // Count array to store the frequency of each character
+        int[] count = new int[26];
+        // Looping through the string array
+        for(int i=0; i<n; i++){
+            // Looping through each character of the string
+            for(int j=0; j<s[i].length(); j++){
+                // Incrementing the count of the character
+                count[s[i].charAt(j) - 'a']++;
             }
         }
-        return true;
+        // Count array to store the frequency of each character in the string
+        // for n number of times
+        int[] count1 = new int[26];
+        // Looping through the string array
+        for(int i=0; i<n; i++){
+            // Looping through each character of the string
+            for(int j=0; j<s[i].length(); j++){
+                // Incrementing the count of the character
+                count1[s[i].charAt(j) - 'a']++;
+            }
+            // Sorting the count array
+            Arrays.sort(count);
+            // Sorting the count1 array
+            Arrays.sort(count1);
+            // Looping through the count array
+            for(int k=0; k<26; k++){
+                // Checking if the frequency of each character is same in both the strings
+                if(count[k] != count1[k]){
+                    return false;
+                }
+            }
+        }
+        return false;
     }
 }
