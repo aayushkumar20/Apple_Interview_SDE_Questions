4c66d8784c18c6e365e5e2e57d0a1a581e8647e6
aayushkumar20 <aayushkumarjsr16@protonmail.com>
2023-08-10 13:43:22 +0530

    Update Number_Of_One_Bits.swift

--- Number_Of_One_Bits.swift
+++ Number_Of_One_Bits.swift
@@ -1,1 +1,33 @@
-// Number_Of_One_Bits
+// Number Of One Bits
+// Link: https://leetcode.com/problems/number-of-1-bits/
+
+// Write a function that takes the binary representation of an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+// Note:
+
+// Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+// In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
+
+// Example 1:
+// Input: n = 00000000000000000000000000001011
+// Output: 3
+// Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+
+// Example 2:
+// Input: n = 00000000000000000000000010000000
+// Output: 1
+// Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
+
+class Solution {
+    func hammingWeight(_ n: Int) -> Int {
+        var count = 0
+        var num = n
+        
+        while num > 0 {
+            count += num & 1
+            num >>= 1
+        }
+        
+        return count
+    }
+}
