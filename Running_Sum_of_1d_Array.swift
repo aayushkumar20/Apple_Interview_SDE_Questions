193df4f0669426a37c82f5acc020ddff3012bba9
aayushkumar20 <aayushkumarjsr16@protonmail.com>
2023-10-11 19:20:39 +0530

    11th october 2023

---
+++ Running_Sum_of_1d_Array.swift
@@ -0,0 +1,35 @@
+// Running Sum of 1d Array
+// https://leetcode.com/problems/running-sum-of-1d-array/
+
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums.
+
+// Example 1:
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+// Example 2:
+// Input: nums = [1,1,1,1,1]
+// Output: [1,2,3,4,5]
+// Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+// Example 3:
+// Input: nums = [3,1,2,10,1]
+// Output: [3,4,6,16,17]
+
+class Solution {
+    func runningSum(_ nums: [Int]) -> [Int] {
+        var result = [Int]()
+        var sum = 0
+        for num in nums {
+            sum += num
+            result.append(sum)
+        }
+        return result
+    }
+}
+
+// Time complexity: O(n)
+// Space complexity: O(n)
